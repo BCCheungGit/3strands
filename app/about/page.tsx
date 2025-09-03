88497c879493ebbc7ef949f4ecb02883e9be2414
Benjamin Cheung <bc3431@nyu.edu>
2025-09-03 08:35:43 +0800

    reorganized code structure

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
 import NavBar from "../components/navbar";
-import { WhoCard } from "./components/cards";
+import { WhoCard } from "@/components/cards";
 export default function About() {
   return (
     <div className="flex flex-col min-h-screen mx-10">
