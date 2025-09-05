a57d89440724971e34d2effe5a8a23eed6e44be5
Benjamin Cheung <bc3431@nyu.edu>
2025-09-05 14:05:47 +0800

    added jpg files and fixed formatting of about page

--- components/cards.tsx
+++ components/cards.tsx
@@ -1,12 +1,13 @@
 import { BlurFade } from "@/components/magicui/blur-fade";
 import Image from "next/image";
 import { aboutCards } from "@/app/constants";
+
 export function WhoCard() {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center gap-10 w-full mt-10 gap-4 ">
-        {aboutCards.map((card, index) => (
-          <BlurFade key={index} delay={0.5}>
+    <div className="flex flex-col items-center justify-center gap-10 w-full mt-10 gap-4">
+      {aboutCards.map((card, index) =>
+        index % 2 === 0 ? (
+          <BlurFade key={card.index ?? index} delay={0.5}>
             <div className="flex sm:flex-row flex-col items-center justify-center gap-10 max-w-6xl">
               <div className="flex flex-col justify-center items-center gap-4">
                 <h2 className="font-bold fustat text-2xl sm:text-4xl">
@@ -25,8 +26,28 @@ export function WhoCard() {
               )}
             </div>
           </BlurFade>
-        ))}{" "}
-      </div>
-    </>
+        ) : (
+          <BlurFade key={card.index ?? index} delay={0.5}>
+            <div className="flex sm:flex-row flex-col items-center justify-center gap-10 max-w-6xl">
+              {card.image && (
+                <Image
+                  src={card.image}
+                  alt=""
+                  width={400}
+                  height={400}
+                  className="sm:w-[500px] sm:h-[500px] w-[300px] h-[400px]"
+                />
+              )}
+              <div className="flex flex-col justify-center items-center gap-4">
+                <h2 className="font-bold fustat text-2xl sm:text-4xl">
+                  {card.title}
+                </h2>
+                <p className="text-lg sm:text-xl fustat">{card.description}</p>
+              </div>
+            </div>
+          </BlurFade>
+        ),
+      )}
+    </div>
   );
 }
