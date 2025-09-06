eec77558d0f63b9a732fc4f1e9346a15126fb83e
Benjamin Cheung <bc3431@nyu.edu>
2025-09-06 17:03:41 +0800

    added new photos and refactored about page for mobile-friendly layout

--- components/cards.tsx
+++ components/cards.tsx
@@ -4,50 +4,33 @@ import { aboutCards } from "@/app/constants";
 
 export function WhoCard() {
   return (
-    <div className="flex flex-col items-center justify-center gap-10 w-full mt-10 gap-4">
-      {aboutCards.map((card, index) =>
-        index % 2 === 0 ? (
-          <BlurFade key={card.index ?? index} delay={0.5}>
-            <div className="flex sm:flex-row flex-col items-center justify-center gap-10 max-w-6xl">
-              <div className="flex flex-col justify-center items-center gap-4">
-                <h2 className="font-bold fustat text-2xl sm:text-4xl">
-                  {card.title}
-                </h2>
-                <p className="text-lg sm:text-xl fustat">{card.description}</p>
-              </div>
-              {card.image && (
-                <Image
-                  src={card.image}
-                  alt=""
-                  width={400}
-                  height={400}
-                  className="sm:w-[500px] sm:h-[500px] w-[300px] h-[400px]"
-                />
-              )}
+    <div className="flex flex-col items-center justify-center gap-10 w-full mt-10">
+      {aboutCards.map((card, index) => (
+        <BlurFade key={card.index ?? index} delay={0.5}>
+          <div
+            className={`flex flex-col md:flex-row items-center justify-center gap-4 md:gap-10 max-w-6xl ${
+              index % 2 !== 0 ? "md:flex-row-reverse" : ""
+            }`}
+          >
+            <div className="flex flex-col justify-center items-center gap-4 text-center md:text-left">
+              <h2 className="font-bold fustat text-2xl sm:text-4xl">
+                {card.title}
+              </h2>
+              <p className="text-lg sm:text-xl fustat">{card.description}</p>
             </div>
-          </BlurFade>
-        ) : (
-          <BlurFade key={card.index ?? index} delay={0.5}>
-            <div className="flex sm:flex-row flex-col items-center justify-center gap-10 max-w-6xl">
-              {card.image && (
-                <Image
-                  src={card.image}
-                  alt=""
-                  width={400}
-                  height={400}
-                  className="sm:w-[500px] sm:h-[500px] w-[300px] h-[400px]"
-                />
-              )}
-              <div className="flex flex-col justify-center items-center gap-4">
-                <h2 className="font-bold fustat text-2xl sm:text-4xl">
-                  {card.title}
-                </h2>
-                <p className="text-lg sm:text-xl fustat">{card.description}</p>
-              </div>
-            </div>
-          </BlurFade>
-        ),
-      )}
+
+            {card.image && (
+              <Image
+                src={card.image}
+                alt={card.title}
+                width={400}
+                height={400}
+                className="w-[500px] h-[500px] object-contain"
+              />
+            )}
+          </div>
+        </BlurFade>
+      ))}
     </div>
   );
 }
