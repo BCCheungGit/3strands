6fd17f7de15357d0c4ea5d07ab47ea64834a4b37
Benjamin Cheung <bc3431@nyu.edu>
2025-09-09 14:45:40 +0800

    added instagram link to bottom bar

--- app/components/BottomBar.tsx
+++ app/components/BottomBar.tsx
@@ -1,13 +1,29 @@
 "use client";
 import { Copyright } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 export function BottomBar() {
   return (
     <nav className="w-full py-20 mt-10 flex items-center justify-center bottom-0 z-20 border-t-1">
-      <div className="w-full gap-10 flex px-20 justify-between items-center">
+      <div className="w-full gap-10 flex px-10 sm:px-20 justify-between items-center">
         <div className="flex fustat flex-row text-nowrap items-center justify-center gap-1 text-[12px]">
           <Copyright size={16} />
           3Strands. 2025
+        </div>
+        <div className="flex text-nowrap flex-row items-center justify-center gap-2 fustat">
+          <p>Find us on</p>
+          <a
+            href="https://www.instagram.com/threestrandsclub?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+            target="_blank"
+          >
+            <Image
+              src="/instagram.png"
+              alt="instagram logo"
+              height={50}
+              width={50}
+              className="sm:w-8 sm:h-8 w-4 h-4 cursor-pointer hover:opacity-70"
+            />
+          </a>
         </div>
         <Button
           variant="link"
